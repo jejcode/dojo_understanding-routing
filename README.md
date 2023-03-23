d6ae662f77d1bd588dc41b3376a3a02289366df2
Joel Jacobson <joel.e.jacobson@gmail.com>
2023-03-23 08:22:26 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Understanding of FLASK routing
+Joel Jacobson
+
+What I learned:
+We can handle errors by using the @app.errorhandler(404) route
+
+I still need more practice to be able to construct the basic ouline of the server.py file
---
+++ server.py
@@ -0,0 +1,25 @@
+from flask import Flask #import Flask class
+app = Flask(__name__) # the new instance of Flask is called 'app'
+
+@app.route('/') # names the route for flask framework
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/dojo') # route named dojo
+def dojo():
+    return 'Dojo!'
+
+@app.route('/say/<string:name>') #must be a string
+def say_hi(name):
+    return f"Hi, {name.capitalize()}!" # case corrects proper nouns
+
+@app.route('/repeat/<int:num>/<string:msg>')
+def repeat_msg(num,msg):
+    return msg * num
+
+@app.errorhandler(404) # customize response to particular errors
+def page_not_found(e): # e is the actual error message 
+    return f"Sorry! No response. Try again."
+
+if __name__ == "__main__":
+    app.run(debug=True, host='localhost', port=8000)
